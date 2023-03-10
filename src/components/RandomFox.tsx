faee56c8f7b82c4e7099bf341342badd01db2dd2
Diego <dgogo9@hotmail.com>
2023-03-09 18:32:52 -0600

    Add lazy loading

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type Props = {
   image: string;
@@ -6,15 +6,34 @@ type Props = {
 
 export const RandomFox = (props: Props): JSX.Element => {
   const { image } = props;
+  const [src, setSrc] = useState(
+    "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+  );
   const ref = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setSrc(image);
+        }
+      });
+    });
+
+    if (ref.current) observer.observe(ref.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [image]);
 
   return (
     <img
       ref={ref}
-      src={image}
+      src={src}
       width={320}
       height="auto"
-      className="rounded-r"
+      className="rounded-r bg-gray-300"
     />
   );
 };
