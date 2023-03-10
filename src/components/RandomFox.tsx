3a43acb161dead4a47a1be6f3a119f9677660a76
Diego <dgogo9@hotmail.com>
2023-03-09 18:11:18 -0600

    useRef typing

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,9 +1,20 @@
+import { useRef } from "react";
+
 type Props = {
   image: string;
 };
 
 export const RandomFox = (props: Props): JSX.Element => {
   const { image } = props;
+  const ref = useRef<HTMLImageElement>(null);
 
-  return <img src={image} width={320} height="auto" className="rounded-r" />;
+  return (
+    <img
+      ref={ref}
+      src={image}
+      width={320}
+      height="auto"
+      className="rounded-r"
+    />
+  );
 };
